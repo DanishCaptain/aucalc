98f81d474eaeacb549eaa0239987676f4cd0c8e9
Danish Captain <brian@bombholtmagic.com>
2019-04-04 23:10:02 -0700

    added task

--- app/src/main/java/org/mendybot/android/aucalc/MainActivity.java
+++ app/src/main/java/org/mendybot/android/aucalc/MainActivity.java
@@ -13,8 +13,7 @@ import org.mendybot.android.aucalc.model.CommoditiesModel;
 import org.mendybot.android.aucalc.sync.CommoditySyncService;
 
 public class MainActivity extends AppCompatActivity implements CommoditiesChangeListener {
-    private static final int JOB_ID1 = 11220;
-    private static final int JOB_ID2 = 11221;
+    private static final int JOB_ID = 11220;
     private EditText etAuOzt;
     private EditText etAuG;
 
@@ -24,29 +23,17 @@ public class MainActivity extends AppCompatActivity implements CommoditiesChange
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        JobInfo jobInfo1 = new JobInfo.Builder(JOB_ID1, new ComponentName(this, CommoditySyncService.class))
-                .setPeriodic(10)
-                .build();
-        JobInfo jobInfo2 = new JobInfo.Builder(JOB_ID2, new ComponentName(this, CommoditySyncService.class))
-//                .setPeriodic(60*1000)
-                .setPeriodic(6000)
+        JobInfo jobInfo = new JobInfo.Builder(JOB_ID, new ComponentName(this, CommoditySyncService.class))
+                .setPeriodic(60*1000)
+//                .setPeriodic(6000)
                 //.setPeriodic(15*60*1000,7*60*1000)
                 .build();
 
         final JobScheduler jobScheduler = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
-        int x1 = jobScheduler.schedule(jobInfo1);
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-        }
-        jobScheduler.cancel(JOB_ID1);
-        int x2 = jobScheduler.schedule(jobInfo2);
-
+        int x = jobScheduler.schedule(jobInfo);
         etAuOzt = (EditText) findViewById(R.id.au_price_ozt);
         etAuG = (EditText) findViewById(R.id.au_price_g);
-
         CommoditiesModel.getInstance().addCommoditiesChangeListener(this);
-
     }
 
     @Override
